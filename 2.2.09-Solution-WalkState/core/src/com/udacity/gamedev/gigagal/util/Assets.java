3d5326c740df17d1d739787de9dca75f5eb3a5d2
Jeremy Silver <jeremy@udacity.com>
2015-11-10 11:31:03 -0800

    Add 2.2.09

---
+++ 2.2.09-Solution-WalkState/core/src/com/udacity/gamedev/gigagal/util/Assets.java
@@ -0,0 +1,62 @@
+package com.udacity.gamedev.gigagal.util;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.assets.AssetDescriptor;
+import com.badlogic.gdx.assets.AssetErrorListener;
+import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas.AtlasRegion;
+import com.sun.media.jfxmediaimpl.MediaDisposer.Disposable;
+
+
+public class Assets implements Disposable, AssetErrorListener {
+
+    public static final String TAG = Assets.class.getName();
+    public static final Assets instance = new Assets();
+    public GigaGalAssets gigaGalAssets;
+    private AssetManager assetManager;
+
+    private Assets() {
+    }
+
+    public void init(AssetManager assetManager) {
+        this.assetManager = assetManager;
+        assetManager.setErrorListener(this);
+        assetManager.load(Constants.TEXTURE_ATLAS, TextureAtlas.class);
+        assetManager.finishLoading();
+
+        TextureAtlas atlas = assetManager.get(Constants.TEXTURE_ATLAS);
+        gigaGalAssets = new GigaGalAssets(atlas);
+    }
+
+    @Override
+    public void error(AssetDescriptor asset, Throwable throwable) {
+        Gdx.app.error(TAG, "Couldn't load asset: " + asset.fileName, throwable);
+    }
+
+    @Override
+    public void dispose() {
+        assetManager.dispose();
+    }
+
+    public class GigaGalAssets {
+
+        public final AtlasRegion standingLeft;
+        public final AtlasRegion standingRight;
+        public final AtlasRegion walkingLeft;
+        public final AtlasRegion walkingRight;
+        public final AtlasRegion jumpingLeft;
+        public final AtlasRegion jumpingRight;
+
+
+        public GigaGalAssets(TextureAtlas atlas) {
+            standingLeft = atlas.findRegion(Constants.STANDING_LEFT);
+            standingRight = atlas.findRegion(Constants.STANDING_RIGHT);
+            walkingLeft = atlas.findRegion(Constants.WALKING_LEFT_2);
+            walkingRight = atlas.findRegion(Constants.WALKING_RIGHT_2);
+
+            jumpingLeft = atlas.findRegion(Constants.JUMPING_LEFT);
+            jumpingRight = atlas.findRegion(Constants.JUMPING_RIGHT);
+        }
+    }
+}
