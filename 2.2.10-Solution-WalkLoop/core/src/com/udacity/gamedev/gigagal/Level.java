2a34e553edf1b8ebb2ad63f32c13f97d15e292a6
Jeremy Silver <jeremy@udacity.com>
2015-11-10 11:27:15 -0800

    Finish 2.2.10 solution

--- 2.2.10-Solution-WalkLoop/core/src/com/udacity/gamedev/gigagal/Level.java
+++ 2.2.10-Solution-WalkLoop/core/src/com/udacity/gamedev/gigagal/Level.java
@@ -1,36 +1,21 @@
 package com.udacity.gamedev.gigagal;
 
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
-import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
-import com.badlogic.gdx.utils.Array;
 import com.udacity.gamedev.gigagal.entities.GigaGal;
-import com.udacity.gamedev.gigagal.entities.Platform;
 
 public class Level {
 
     GigaGal gigaGal;
-    Array<Platform> platforms;
 
     public Level() {
         gigaGal = new GigaGal();
-        platforms = new Array<Platform>();
-
-        platforms.add(new Platform(70, 30, 20, 20));
     }
 
     public void update(float delta) {
         gigaGal.update(delta);
     }
 
-    public void render(SpriteBatch batch, ShapeRenderer renderer) {
-
-        renderer.begin(ShapeType.Filled);
-        for (Platform platform : platforms) {
-            platform.render(renderer);
-        }
-        renderer.end();
-
+    public void render(SpriteBatch batch) {
         batch.begin();
         gigaGal.render(batch);
         batch.end();
