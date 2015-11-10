5157cea5497d46de2bd091d352b57896d2c5363f
Jeremy Silver <jeremy@udacity.com>
2015-11-10 14:00:29 -0800

    Add 2.2.02 solution

---
+++ 2.2.02-Solution-TheProjectSkeleton/core/src/com/udacity/gamedev/gigagal/entities/GigaGal.java
@@ -0,0 +1,20 @@
+package com.udacity.gamedev.gigagal.entities;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class GigaGal {
+
+    public final static String TAG = GigaGal.class.getName();
+
+    public GigaGal() {
+
+    }
+
+    public void update(float delta) {
+
+    }
+
+    public void render(SpriteBatch batch) {
+
+    }
+}
