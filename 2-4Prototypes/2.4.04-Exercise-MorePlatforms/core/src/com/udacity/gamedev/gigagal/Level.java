27101cf028403801a054b05a47ecf823b7ed2415
Jeremy Silver <jeremy@udacity.com>
2015-12-18 12:02:00 -0800

    Finish 2.4.04

--- 2-4Prototypes/2.4.04-Exercise-MorePlatforms/core/src/com/udacity/gamedev/gigagal/Level.java
+++ 2-4Prototypes/2.4.04-Exercise-MorePlatforms/core/src/com/udacity/gamedev/gigagal/Level.java
@@ -36,12 +36,6 @@ public class Level {
         platforms.add(new Platform(75, 90, 100, 65));
         platforms.add(new Platform(35, 55, 50, 20));
         platforms.add(new Platform(10, 20, 20, 9));
-        platforms.add(new Platform(100, 110, 30, 9));
-        platforms.add(new Platform(200, 130, 30, 40));
-        platforms.add(new Platform(150, 150, 30, 9));
-        platforms.add(new Platform(150, 180, 30, 9));
-        platforms.add(new Platform(200, 200, 9, 9));
-        platforms.add(new Platform(280, 100, 30, 9));
 
         gigaGal = new GigaGal(new Vector2(15, 40));
 
