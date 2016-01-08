193470e5754229cf9df0649d91de0848febf7dba
Jeremy Silver <jeremy@udacity.com>
2016-01-08 10:38:59 -0800

    Progress on 2.5 exercises

---
+++ 2.5.08-Solution-RecoilingInTerror/core/src/com/udacity/gamedev/gigagal/util/Enums.java
@@ -0,0 +1,21 @@
+package com.udacity.gamedev.gigagal.util;
+
+
+public class Enums {
+
+    public enum Direction {
+        LEFT, RIGHT
+    }
+
+    public enum JumpState {
+        JUMPING,
+        FALLING,
+        GROUNDED,
+        RECOILING
+    }
+
+    public enum WalkState {
+        STANDING,
+        WALKING
+    }
+}
