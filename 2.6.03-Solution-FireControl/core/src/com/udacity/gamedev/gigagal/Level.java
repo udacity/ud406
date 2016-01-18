3505ad4a5a4ca89ff56d5fa0ed0f3ba1feb16f28
Jeremy Silver <jeremy@udacity.com>
2016-01-17 17:44:12 -0800

    Progress on 2-5/2-6 exercises

---
+++ 2.6.03-Solution-FireControl/core/src/com/udacity/gamedev/gigagal/Level.java
@@ -0,0 +1,119 @@
+package com.udacity.gamedev.gigagal;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.utils.Array;
+import com.badlogic.gdx.utils.DelayedRemovalArray;
+import com.badlogic.gdx.utils.viewport.Viewport;
+import com.udacity.gamedev.gigagal.entities.Bullet;
+import com.udacity.gamedev.gigagal.entities.Enemy;
+import com.udacity.gamedev.gigagal.entities.GigaGal;
+import com.udacity.gamedev.gigagal.entities.Platform;
+import com.udacity.gamedev.gigagal.util.Enums.Direction;
+
+public class Level {
+
+    public static final String TAG = Level.class.getName();
+
+    private Viewport viewport;
+
+    private GigaGal gigaGal;
+    private Array<Platform> platforms;
+    private DelayedRemovalArray<Enemy> enemies;
+    private DelayedRemovalArray<Bullet> bullets;
+
+
+    public Level(Viewport viewport) {
+        this.viewport = viewport;
+        initializeDebugLevel();
+    }
+
+    public void update(float delta) {
+        // Update GigaGal
+        gigaGal.update(delta, platforms);
+
+        // Update Bullets
+        bullets.begin();
+        for (Bullet bullet : bullets) {
+            bullet.update(delta);
+            if (!bullet.active) {
+                bullets.removeValue(bullet, false);
+            }
+        }
+        bullets.end();
+
+        // Update Enemies
+        for (int i = 0; i < enemies.size; i++) {
+            Enemy enemy = enemies.get(i);
+            enemy.update(delta);
+        }
+    }
+
+    public void render(SpriteBatch batch) {
+
+        for (Platform platform : platforms) {
+            platform.render(batch);
+        }
+
+
+        for (Enemy enemy : enemies) {
+            enemy.render(batch);
+        }
+
+        gigaGal.render(batch);
+
+        for (Bullet bullet : bullets) {
+            bullet.render(batch);
+        }
+
+    }
+
+    private void initializeDebugLevel() {
+
+        gigaGal = new GigaGal(new Vector2(15, 40), this);
+
+        platforms = new Array<Platform>();
+        bullets = new DelayedRemovalArray<Bullet>();
+        enemies = new DelayedRemovalArray<Enemy>();
+
+        platforms.add(new Platform(15, 100, 30, 20));
+
+        Platform enemyPlatform = new Platform(75, 90, 100, 65);
+        enemies.add(new Enemy(enemyPlatform));
+
+        platforms.add(enemyPlatform);
+        platforms.add(new Platform(35, 55, 50, 20));
+        platforms.add(new Platform(10, 20, 20, 9));
+
+    }
+
+    public Array<Platform> getPlatforms() {
+        return platforms;
+    }
+
+    public DelayedRemovalArray<Enemy> getEnemies() {
+        return enemies;
+    }
+
+    public Viewport getViewport() {
+        return viewport;
+    }
+
+    public void setViewport(Viewport viewport) {
+        this.viewport = viewport;
+    }
+
+    public GigaGal getGigaGal() {
+        return gigaGal;
+    }
+
+    public void setGigaGal(GigaGal gigaGal) {
+        this.gigaGal = gigaGal;
+    }
+
+    public void spawnBullet(Vector2 position, Direction direction) {
+        bullets.add(new Bullet(position, direction));
+    }
+
+
+}
---
+++ 2.6.04-Exercise-PoliceThePlasma/core/src/com/udacity/gamedev/gigagal/entities/Bullet.java
@@ -0,0 +1,43 @@
+package com.udacity.gamedev.gigagal.entities;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Vector2;
+import com.udacity.gamedev.gigagal.Level;
+import com.udacity.gamedev.gigagal.util.Assets;
+import com.udacity.gamedev.gigagal.util.Constants;
+import com.udacity.gamedev.gigagal.util.Enums.Direction;
+import com.udacity.gamedev.gigagal.util.Utils;
+
+public class Bullet {
+
+    private final Direction direction;
+    private final Level level;
+    public boolean active;
+    private Vector2 position;
+
+    public Bullet(Level level, Vector2 position, Direction direction) {
+        this.level = level;
+        this.position = position;
+        this.direction = direction;
+        active = true;
+    }
+
+    public void update(float delta) {
+        switch (direction) {
+            case LEFT:
+                position.x -= delta * Constants.BULLET_MOVE_SPEED;
+                break;
+            case RIGHT:
+                position.x += delta * Constants.BULLET_MOVE_SPEED;
+                break;
+        }
+
+
+    }
+
+    public void render(SpriteBatch batch) {
+        TextureRegion region = Assets.instance.bulletAssets.bullet;
+        Utils.drawTextureRegion(batch, region, position, Constants.BULLET_CENTER);
+    }
+}
