7e7ea7c14ff1c98e4b8b100fd753a62e74150a11
Jeremy Silver <jeremy@udacity.com>
2016-01-17 17:30:55 -0800

    Add initial 2.6.04 solution

---
+++ 2.6.04-Solution-PoliceThePlasma/core/src/com/udacity/gamedev/gigagal/entities/GigaGal.java
@@ -0,0 +1,254 @@
+package com.udacity.gamedev.gigagal.entities;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input.Keys;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.utils.Array;
+import com.badlogic.gdx.utils.TimeUtils;
+import com.udacity.gamedev.gigagal.Level;
+import com.udacity.gamedev.gigagal.util.Assets;
+import com.udacity.gamedev.gigagal.util.Constants;
+import com.udacity.gamedev.gigagal.util.Enums;
+import com.udacity.gamedev.gigagal.util.Enums.Direction;
+import com.udacity.gamedev.gigagal.util.Enums.JumpState;
+import com.udacity.gamedev.gigagal.util.Enums.WalkState;
+import com.udacity.gamedev.gigagal.util.Utils;
+
+public class GigaGal {
+
+    public final static String TAG = GigaGal.class.getName();
+
+    Vector2 spawnLocation;
+    Vector2 position;
+    Vector2 lastFramePosition;
+    Vector2 velocity;
+
+    Direction facing;
+    JumpState jumpState;
+    WalkState walkState;
+
+    long walkStartTime;
+    long jumpStartTime;
+
+    Level level;
+
+    public GigaGal(Vector2 spawnLocation, Level level) {
+        this.spawnLocation = spawnLocation;
+        this.level = level;
+        position = new Vector2();
+        lastFramePosition = new Vector2();
+        velocity = new Vector2();
+        init();
+    }
+
+
+    public void init() {
+        position.set(spawnLocation);
+        lastFramePosition.set(spawnLocation);
+        velocity.setZero();
+        jumpState = Enums.JumpState.FALLING;
+        facing = Direction.RIGHT;
+        walkState = Enums.WalkState.NOT_WALKING;
+    }
+
+    public Vector2 getPosition() {
+        return position;
+    }
+
+    public void update(float delta, Array<Platform> platforms) {
+
+        lastFramePosition.set(position);
+        velocity.y -= Constants.GRAVITY;
+        position.mulAdd(velocity, delta);
+
+        if (position.y < Constants.KILL_PLANE) {
+            init();
+        }
+
+        // Land on/fall off platforms
+        if (jumpState != Enums.JumpState.JUMPING) {
+            if (jumpState != JumpState.RECOILING) {
+                jumpState = Enums.JumpState.FALLING;
+            }
+
+            for (Platform platform : platforms) {
+                if (landedOnPlatform(platform)) {
+                    jumpState = Enums.JumpState.GROUNDED;
+                    velocity.y = 0;
+                    velocity.x = 0;
+                    position.y = platform.top + Constants.GIGAGAL_EYE_HEIGHT;
+                }
+            }
+        }
+
+        // Collide with enemies
+
+        Rectangle gigaGalBounds = new Rectangle(
+                position.x - Constants.GIGAGAL_STANCE_WIDTH / 2,
+                position.y - Constants.GIGAGAL_EYE_HEIGHT,
+                Constants.GIGAGAL_STANCE_WIDTH,
+                Constants.GIGAGAL_HEIGHT);
+
+        for (Enemy enemy : level.getEnemies()) {
+            Rectangle enemyBounds = new Rectangle(
+                    enemy.position.x - Constants.ENEMY_COLLISION_RADIUS,
+                    enemy.position.y - Constants.ENEMY_COLLISION_RADIUS,
+                    2 * Constants.ENEMY_COLLISION_RADIUS,
+                    2 * Constants.ENEMY_COLLISION_RADIUS
+            );
+            if (gigaGalBounds.overlaps(enemyBounds)) {
+
+                if (position.x < enemy.position.x) {
+                    recoilFromEnemy(Direction.LEFT);
+                } else {
+                    recoilFromEnemy(Direction.RIGHT);
+                }
+            }
+        }
+
+        // Move left/right
+        if (jumpState != JumpState.RECOILING) {
+            if (Gdx.input.isKeyPressed(Keys.LEFT)) {
+                moveLeft(delta);
+            } else if (Gdx.input.isKeyPressed(Keys.RIGHT)) {
+                moveRight(delta);
+            } else {
+                walkState = Enums.WalkState.NOT_WALKING;
+            }
+        }
+
+        // Jump
+        if (Gdx.input.isKeyPressed(Keys.Z)) {
+            switch (jumpState) {
+                case GROUNDED:
+                    startJump();
+                    break;
+                case JUMPING:
+                    continueJump();
+            }
+        } else {
+            endJump();
+        }
+
+
+        // Shoot
+        if (Gdx.input.isKeyJustPressed(Keys.X)) {
+
+            Vector2 bulletPosition;
+
+            if (facing == Direction.RIGHT) {
+                bulletPosition = new Vector2(
+                        position.x + Constants.GIGAGAL_CANNON_OFFSET.x,
+                        position.y + Constants.GIGAGAL_CANNON_OFFSET.y
+                );
+            } else {
+                bulletPosition = new Vector2(
+                        position.x - Constants.GIGAGAL_CANNON_OFFSET.x,
+                        position.y + Constants.GIGAGAL_CANNON_OFFSET.y
+                );
+            }
+            level.spawnBullet(bulletPosition, facing);
+        }
+    }
+
+    boolean landedOnPlatform(Platform platform) {
+        boolean leftFootIn = false;
+        boolean rightFootIn = false;
+        boolean straddle = false;
+
+        if (lastFramePosition.y - Constants.GIGAGAL_EYE_HEIGHT >= platform.top &&
+                position.y - Constants.GIGAGAL_EYE_HEIGHT < platform.top) {
+
+            float leftFoot = position.x - Constants.GIGAGAL_STANCE_WIDTH / 2;
+            float rightFoot = position.x + Constants.GIGAGAL_STANCE_WIDTH / 2;
+
+            leftFootIn = (platform.left < leftFoot && platform.right > leftFoot);
+            rightFootIn = (platform.left < rightFoot && platform.right > rightFoot);
+            straddle = (platform.left > leftFoot && platform.right < rightFoot);
+        }
+        return leftFootIn || rightFootIn || straddle;
+    }
+
+
+    private void moveLeft(float delta) {
+        if (jumpState == Enums.JumpState.GROUNDED && walkState != Enums.WalkState.WALKING) {
+            walkStartTime = TimeUtils.nanoTime();
+        }
+        walkState = Enums.WalkState.WALKING;
+        facing = Direction.LEFT;
+        position.x -= delta * Constants.GIGAGAL_MOVE_SPEED;
+    }
+
+    private void moveRight(float delta) {
+        if (jumpState == Enums.JumpState.GROUNDED && walkState != Enums.WalkState.WALKING) {
+            walkStartTime = TimeUtils.nanoTime();
+        }
+        walkState = Enums.WalkState.WALKING;
+        facing = Direction.RIGHT;
+        position.x += delta * Constants.GIGAGAL_MOVE_SPEED;
+    }
+
+    private void startJump() {
+        jumpState = Enums.JumpState.JUMPING;
+        jumpStartTime = TimeUtils.nanoTime();
+        continueJump();
+    }
+
+    private void continueJump() {
+        if (jumpState == Enums.JumpState.JUMPING) {
+            if (Utils.secondsSince(jumpStartTime) < Constants.MAX_JUMP_DURATION) {
+                velocity.y = Constants.JUMP_SPEED;
+            } else {
+                endJump();
+            }
+        }
+    }
+
+    private void endJump() {
+        if (jumpState == Enums.JumpState.JUMPING) {
+            jumpState = Enums.JumpState.FALLING;
+        }
+    }
+
+    private void recoilFromEnemy(Direction direction) {
+
+        jumpState = JumpState.RECOILING;
+        velocity.y = Constants.KNOCKBACK_VELOCITY.y;
+
+        if (direction == Direction.LEFT) {
+            velocity.x = -Constants.KNOCKBACK_VELOCITY.x;
+        } else {
+            velocity.x = Constants.KNOCKBACK_VELOCITY.x;
+        }
+    }
+
+    public void render(SpriteBatch batch) {
+        TextureRegion region = Assets.instance.gigaGalAssets.standingRight;
+
+        if (facing == Direction.RIGHT && jumpState != Enums.JumpState.GROUNDED) {
+            region = Assets.instance.gigaGalAssets.jumpingRight;
+        } else if (facing == Direction.RIGHT && walkState == Enums.WalkState.NOT_WALKING) {
+            region = Assets.instance.gigaGalAssets.standingRight;
+        } else if (facing == Direction.RIGHT && walkState == Enums.WalkState.WALKING) {
+            float walkTimeSeconds = Utils.secondsSince(walkStartTime);
+            region = Assets.instance.gigaGalAssets.walkingRightAnimation.getKeyFrame(walkTimeSeconds);
+        } else if (facing == Direction.LEFT && jumpState != Enums.JumpState.GROUNDED) {
+            region = Assets.instance.gigaGalAssets.jumpingLeft;
+        } else if (facing == Direction.LEFT && walkState == Enums.WalkState.NOT_WALKING) {
+            region = Assets.instance.gigaGalAssets.standingLeft;
+        } else if (facing == Direction.LEFT && walkState == Enums.WalkState.WALKING) {
+            float walkTimeSeconds = Utils.secondsSince(walkStartTime);
+            region = Assets.instance.gigaGalAssets.walkingLeftAnimation.getKeyFrame(walkTimeSeconds);
+        }
+
+        Utils.drawTextureRegion(batch, region,
+                position.x - Constants.GIGAGAL_EYE_POSITION.x,
+                position.y - Constants.GIGAGAL_EYE_POSITION.y
+        );
+
+    }
+
+}
---
+++ 2.6.04-Solution-PoliceThePlasma/core/src/com/udacity/gamedev/gigagal/util/Constants.java
@@ -0,0 +1,70 @@
+package com.udacity.gamedev.gigagal.util;
+
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.math.Vector2;
+
+public class Constants {
+
+    // World/Camera
+    public static final Color BACKGROUND_COLOR = Color.SKY;
+    public static final float WORLD_SIZE = 160;
+    public static final float KILL_PLANE = -100;
+    public static final float GRAVITY = 10;
+    public static final float CHASE_CAM_MOVE_SPEED = WORLD_SIZE;
+
+    public static final String TEXTURE_ATLAS = "images/gigagal.pack.atlas";
+
+
+    // GigaGal
+    public static final Vector2 GIGAGAL_EYE_POSITION = new Vector2(16, 24);
+    public static final float GIGAGAL_EYE_HEIGHT = 16.0f;
+    public static final float GIGAGAL_STANCE_WIDTH = 21.0f;
+    public static final Vector2 GIGAGAL_CANNON_OFFSET = new Vector2(12, -7);
+    public static final float GIGAGAL_HEIGHT = 23.0f;
+    public static final float GIGAGAL_MOVE_SPEED = 100;
+
+    public static final float JUMP_SPEED = 200;
+    public static final Vector2 KNOCKBACK_VELOCITY = new Vector2(200, 200);
+    public static final float MAX_JUMP_DURATION = .1f;
+
+    public static final String STANDING_RIGHT = "standing-right";
+    public static final String STANDING_LEFT = "standing-left";
+    public static final String JUMPING_RIGHT = "jumping-right";
+    public static final String JUMPING_LEFT = "jumping-left";
+    public static final String WALKING_RIGHT_1 = "walk-1-right";
+    public static final String WALKING_LEFT_1 = "walk-1-left";
+    public static final String WALKING_RIGHT_2 = "walk-2-right";
+    public static final String WALKING_LEFT_2 = "walk-2-left";
+    public static final String WALKING_RIGHT_3 = "walk-3-right";
+    public static final String WALKING_LEFT_3 = "walk-3-left";
+    public static final float WALK_LOOP_DURATION = 0.25f;
+
+    // Platform
+    public static final String PLATFORM_SPRITE = "platform";
+    public static final int PLATFORM_EDGE = 8;
+
+    // Enemy
+    public static final String ENEMY_SPRITE = "enemy";
+    public static final Vector2 ENEMY_CENTER = new Vector2(14, 22);
+    public static final float ENEMY_MOVEMENT_SPEED = 10;
+    public static final float ENEMY_BOB_AMPLITUDE = 2;
+    public static final float ENEMY_BOB_PERIOD = 3.0f;
+    public static final float ENEMY_COLLISION_RADIUS = 15;
+
+    // Bullet
+    public static final String BULLET_SPRITE = "bullet";
+    public static final float BULLET_MOVE_SPEED = 150;
+    public static final Vector2 BULLET_CENTER = new Vector2(3, 2);
+
+
+    // Explosion
+    public static final String EXPLOSION_LARGE = "explosion-large";
+    public static final String EXPLOSION_MEDIUM = "explosion-medium";
+    public static final String EXPLOSION_SMALL = "explosion-small";
+    public static final Vector2 EXPLOSION_CENTER = new Vector2(8, 8);
+    public static final float EXPLOSION_DURATION = 0.5f;
+
+    // Powerup
+    public static final String POWERUP_SPRITE = "powerup";
+
+}
