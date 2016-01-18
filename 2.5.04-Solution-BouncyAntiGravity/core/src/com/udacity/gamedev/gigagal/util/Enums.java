5bb46145dc156fb8c7e508f23a3dafbaaf5a68c8
Jeremy Silver <jeremy@udacity.com>
2016-01-18 12:39:14 -0800

    Progress on 2-5 exercises

---
+++ 2.5.04-Solution-BouncyAntiGravity/core/src/com/udacity/gamedev/gigagal/util/Enums.java
@@ -0,0 +1,21 @@
+package com.udacity.gamedev.gigagal.util;
+
+
+public class Enums {
+
+
+    public enum Direction {
+        LEFT, RIGHT
+    }
+
+    public enum JumpState {
+        JUMPING,
+        FALLING,
+        GROUNDED
+    }
+
+    public enum WalkState {
+        NOT_WALKING,
+        WALKING
+    }
+}
--- 2.5.05-Solution-RectanglesCollidingWithCircles/core/src/com/udacity/gamedev/rectanglecirclecollision/RectangleCircleCollisionGame.java
+++ 2.5.05-Solution-RectanglesCollidingWithCircles/core/src/com/udacity/gamedev/rectanglecirclecollision/RectangleCircleCollisionGame.java
@@ -1,27 +1,11 @@
 package com.udacity.gamedev.rectanglecirclecollision;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.Game;
 
-public class RectangleCircleCollisionGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-	}
+public class RectangleCircleCollisionGame extends Game {
 
-	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
+    @Override
+    public void create() {
+        setScreen(new RectangleCircleCollisionScreen());
+    }
 }
