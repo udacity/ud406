983a87018ad74f2666c5caf7c3e450329209fb1c
Jeremy Silver <jeremy@udacity.com>
2016-02-09 12:01:02 -0800

    Finish 2.5.01

--- 2.5.01-Exercise-EnemySprites/README.md
+++ 2.5.01-Exercise-EnemySprites/README.md
@@ -1,3 +1,14 @@
-# 2D Game Development with LibGDX
+# The Enemies
 
-Placeholder to set up instructor note links.
+Let's specify the behavior of our enemies. Each enemy will have a particular platform that they're associated with, and they'll patrol from one end of the platform to the other, then turn around and come back. Since they seem to be floating using some sort of anti-gravity, let's also have them bob up and down a bit as they move.
+
+Next we need to decide how to make our enemies dangerous. We could create some sort of health system, and subtract GigaGal's health when she touches an enemy. What I think would be fun though is for GigaGal to go flying back when she touches an enemy, possibly falling off all the platforms to her certain doom.
+
+
+# Enemy Sprites Sighted!
+
+First things first, setting up and drawing the enemy assets.
+
+This process should be familiar at this point. We need to set up a inner class for the EnemyAssets in our Assets singleton, then we'll draw the Enemy sprite in the render method of gameplay screen.
+
+Start off in `Constants.java`, and good luck! Check out the TODOs to find everything you'll need to do.
---
+++ 2.5.01-Exercise-EnemySprites/core/src/com/udacity/gamedev/gigagal/Level.java
@@ -0,0 +1,81 @@
+package com.udacity.gamedev.gigagal;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.utils.Array;
+import com.badlogic.gdx.utils.viewport.Viewport;
+import com.udacity.gamedev.gigagal.entities.GigaGal;
+import com.udacity.gamedev.gigagal.entities.Platform;
+
+public class Level {
+
+    public static final String TAG = Level.class.getName();
+
+    private Viewport viewport;
+
+    private GigaGal gigaGal;
+    private Array<Platform> platforms;
+
+
+    public Level(Viewport viewport) {
+        this.viewport = viewport;
+        initializeDebugLevel();
+    }
+
+    public void update(float delta) {
+        // Update GigaGal
+        gigaGal.update(delta, platforms);
+
+
+    }
+
+    public void render(SpriteBatch batch) {
+
+        for (Platform platform : platforms) {
+            platform.render(batch);
+        }
+
+        // TODO: Throw an enemy on the screen
+
+
+        gigaGal.render(batch);
+
+
+    }
+
+    private void initializeDebugLevel() {
+
+        gigaGal = new GigaGal(new Vector2(15, 40), this);
+
+        platforms = new Array<Platform>();
+
+        platforms.add(new Platform(15, 100, 30, 20));
+        platforms.add(new Platform(75, 90, 100, 65));
+        platforms.add(new Platform(35, 55, 50, 20));
+        platforms.add(new Platform(10, 20, 20, 9));
+
+    }
+
+    public Array<Platform> getPlatforms() {
+        return platforms;
+    }
+
+
+    public Viewport getViewport() {
+        return viewport;
+    }
+
+    public void setViewport(Viewport viewport) {
+        this.viewport = viewport;
+    }
+
+    public GigaGal getGigaGal() {
+        return gigaGal;
+    }
+
+    public void setGigaGal(GigaGal gigaGal) {
+        this.gigaGal = gigaGal;
+    }
+
+
+}
