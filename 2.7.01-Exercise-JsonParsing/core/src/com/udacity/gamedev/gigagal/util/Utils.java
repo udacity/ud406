aa77d9db4ede3f3b82801128f3bdd917063ee434
Jeremy Silver <jeremy@udacity.com>
2016-03-04 15:49:27 -0800

    Add initial 2.7.01 exercise

---
+++ 2.7.01-Exercise-JsonParsing/core/src/com/udacity/gamedev/gigagal/util/Utils.java
@@ -0,0 +1,42 @@
+package com.udacity.gamedev.gigagal.util;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.utils.TimeUtils;
+
+public class Utils {
+
+    public static void drawTextureRegion(SpriteBatch batch, TextureRegion region, Vector2 position) {
+        drawTextureRegion(batch, region, position.x, position.y);
+    }
+
+    public static void drawTextureRegion(SpriteBatch batch, TextureRegion region, Vector2 position, Vector2 offset) {
+        drawTextureRegion(batch, region, position.x - offset.x, position.y - offset.y);
+    }
+
+    public static void drawTextureRegion(SpriteBatch batch, TextureRegion region, float x, float y) {
+        batch.draw(
+                region.getTexture(),
+                x,
+                y,
+                0,
+                0,
+                region.getRegionWidth(),
+                region.getRegionHeight(),
+                1,
+                1,
+                0,
+                region.getRegionX(),
+                region.getRegionY(),
+                region.getRegionWidth(),
+                region.getRegionHeight(),
+                false,
+                false);
+    }
+
+    public static float secondsSince(long timeNanos) {
+        return MathUtils.nanoToSec * (TimeUtils.nanoTime() - timeNanos);
+    }
+}
